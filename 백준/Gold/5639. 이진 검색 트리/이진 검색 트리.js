e8ae6a8c565a9942b159bdb504b07748ee05b662
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2023-03-23 01:41:20 +0900

    [Gold V] Title: 이진 검색 트리, Time: 328 ms, Memory: 13124 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.js
@@ -0,0 +1,56 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+}
+
+let tree = new BinarySearchTree();
+for(let i=0; i<input.length; i++){
+    tree.insert(input[i])
+}
+console.log(tree.DFSPostOrder().join('\n'))
