1868ebc1598edc85319d1c5e89a96a1d5627ae56
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2023-03-26 01:49:57 +0900

    [Silver II] Title: 최소 힙, Time: 240 ms, Memory: 24068 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -0,0 +1,75 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+
+class MinBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element >= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  extractMin() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild < element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if ((swap === null && rightChild < element) || (swap !== null && rightChild < leftChild)) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+let heap = new MinBinaryHeap();
+let result = '';
+for (let i = 1; i <= input.length; i++) {
+  if (input[i] === 0) {
+    if (heap.values.length === 0) {
+      result += '0' + '\n';
+    } else {
+      result += heap.extractMin() + '\n';
+    }
+  } else {
+    heap.insert(input[i]);
+  }
+}
+console.log(result.slice(0, -1));
