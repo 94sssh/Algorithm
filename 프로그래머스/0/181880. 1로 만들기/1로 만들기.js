c3b17e33cd0d73fe71296c0faf678cf45884fe0b
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-04-07 22:09:06 +0900

    [level 0] Title: 1로 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    return num_list.reduce((acc, cur)=>{
+        while(cur !== 1){
+        cur = cur % 2 === 0 ? cur / 2 : (cur - 1) / 2;
+        acc++;
+        };
+        return acc;
+    }, 0);
+}
