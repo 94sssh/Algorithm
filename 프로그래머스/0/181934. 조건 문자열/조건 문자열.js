dd1fd885cc51235f773134de9e8921d0fbdde9b9
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-04-14 21:40:06 +0900

    [level 0] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,12 +1,12 @@
 function solution(ineq, eq, n, m) {
-    if(ineq==='<'){
+    if(ineq === '<'){
         if(eq==='='){
             return n <= m ? 1 : 0;
         } else {
             return n < m ? 1 : 0;
         }
     } else {
-        if(eq==='='){
+        if(eq === '='){
             return n >= m ? 1 : 0;
         } else {
             return n > m ? 1 : 0;
