6f7ea7428dc69291106d00531dbce89f13bc54c4
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-04-14 21:24:27 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,8 +1,6 @@
 function solution(s) {
-    const answer = [];
-    s.split(' ').forEach((num)=>{
-        if(num === 'Z') return answer.pop();
-        return answer.push(num);
-    })
-    return answer.reduce((a, c) => a + +c, 0);
+    const arr = s.split(' ');
+    return arr.reduce((a, c, idx) =>{
+        return c === 'Z' ? +a - +arr[idx-1] : +a + +c;
+    }, 0)
 }
