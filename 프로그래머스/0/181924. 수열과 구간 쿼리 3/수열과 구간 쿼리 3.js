fb0c68a74ac3f9fc8de3f9bbd8f6e5bbfbda25a2
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-04-28 20:46:17 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.37 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,7 +1,4 @@
 function solution(arr, queries) {
-    queries.forEach(([idx1, idx2])=>{
-        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-    })
-    
+    queries.forEach(([idx1, idx2]) => [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
     return arr;
 }
