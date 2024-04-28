b2580e2d4763e406a3ad3043e3deffb3537ab17c
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-04-28 20:48:55 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,6 +1,6 @@
 function solution(x1, x2, x3, x4) {
-    const down = (x, y) => x === false && y === false ? false : true;
-    const up = (x, y) => x === true && y === true ? true : false;
+    const down = (x, y) => !x && !y ? false : true;
+    const up = (x, y) => !!x && !!y ? true : false;
     
     return up(down(x1, x2), down(x3, x4));
 }
