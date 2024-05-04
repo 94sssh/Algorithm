76b1b19b16c0885d1e0ebea0a3343daa3ecceb6e
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-05-04 13:00:12 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,4 @@
 function solution(emergency) {
-    const order = [...emergency].sort((a,b)=> b-a);
+    const order = [...emergency].sort((a,b)=>b-a);
     return emergency.map((el) => order.indexOf(el)+1);
 }
