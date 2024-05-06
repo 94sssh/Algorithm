bec6bd8aa6e92ce354bd4fd9f5007de2f99de0e9
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-05-06 09:55:57 +0900

    [level 0] Title: 배열 만들기 2, Time: 9.79 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,11 @@
+function solution(l, r) {
+    const answer = [];
+    
+    for(let i=l; i<=r; i++){
+        if([...String(i)].every((el) => el === '0' || el === '5')) answer.push(i);
+    }
+    
+    if(answer.length === 0) answer.push(-1)
+    
+    return answer;
+}
