e1f41ded0ef873aac13971c6c7659af336cbce15
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-05-22 01:17:16 +0900

    [level 0] Title: 2의 영역, Time: 0.38 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,6 +1,6 @@
 function solution(arr) {
     const firstIdx = arr.indexOf(2);
     const lastIdx = arr.lastIndexOf(2);
-    const answer = arr.slice(firstIdx, lastIdx +1);
+    const answer = arr.slice(firstIdx, lastIdx + 1);
     return answer.length === 0 ? [-1] : answer;
 }
