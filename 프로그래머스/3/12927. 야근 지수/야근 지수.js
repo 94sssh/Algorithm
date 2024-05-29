e31fc0a7ea464f46aa852b2ccf27b2330fb24be8
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-05-29 10:28:01 +0900

    [level 3] Title: 야근 지수, Time: 129.11 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,31 +1,9 @@
-function quickSort(arr, left = 0, right = arr.length - 1) {
-  if (left < right) {
-    let pivotIndex = partition(arr, left, right);
-    quickSort(arr, left, pivotIndex - 1);
-    quickSort(arr, pivotIndex + 1, right);
-  }
-  return arr;
-}
-
-function partition(arr, left, right) {
-  const pivot = arr[right];
-  let i = left - 1;
-  for (let j = left; j < right; j++) {
-    if (arr[j] < pivot) {
-      i++;
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-  }
-  [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
-  return i + 1;
-}
-
 function solution(n, works) {
     if (n > works.reduce((a,c) => a+c, 0)) return 0;
 
     const worksMap = works.reduce((a, c) => a.set(c, a.get(c)+1 || 1), new Map());
     while (n > 0 && worksMap.size) {
-        const cur = quickSort([...worksMap.keys()]).at(-1);
+        const cur = [...worksMap.keys()].reduce((a,c) => a > c ? a : c, 1);
         const curVal = worksMap.get(cur);
         const dif = n - curVal;
 
