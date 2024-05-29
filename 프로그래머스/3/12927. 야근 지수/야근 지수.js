2d028275d6da7903daa51638a62c94b173835c95
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-05-29 10:27:18 +0900

    [level 3] Title: 야근 지수, Time: 1128.54 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,21 +1,23 @@
-function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = partition(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
   }
+  return arr;
+}
 
-  const pivot = arr[0];
-  const leftArr = [];
-  const rightArr = [];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      leftArr.push(arr[i]);
-    } else {
-      rightArr.push(arr[i]);
+function partition(arr, left, right) {
+  const pivot = arr[right];
+  let i = left - 1;
+  for (let j = left; j < right; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
   }
-
-  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
+  return i + 1;
 }
 
 function solution(n, works) {
