244e473d826c463bbedd076051caecb23ecc5457
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-06-01 01:15:57 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,7 +1,7 @@
 function solution(n, slicer, num_list) {
-    const [a, b, c] = slicer;
-    if(n === 1) return num_list.slice(0, b+1);
-    if(n === 2) return num_list.slice(a);
-    if(n === 3) return num_list.slice(a, b+1);
-    if(n === 4) return num_list.slice(a, b+1).filter((_, idx)=>idx===0 || idx%c===0);
+    const [s, e, k] = slicer;
+    if(n === 1) return num_list.slice(0, e+1);
+    if(n === 2) return num_list.slice(s);
+    if(n === 3) return num_list.slice(s, e+1);
+    if(n === 4) return num_list.slice(s, e+1).filter((_, idx) => idx===0 || idx%k===0);
 }
