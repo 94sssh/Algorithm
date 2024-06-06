d7e22411af6c63174030c57291cbbbd44210d4a1
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-06-06 10:39:08 +0900

    [level 0] Title: 문자열 묶기, Time: 72.36 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,8 +1,9 @@
 function solution(strArr) {
     const answer = [];
+    strArr.sort((a, b) => b.length - a.length);
     
-    for(let i=1; i<=30; i++){
-        answer.push(strArr.filter((str)=>str.length === i).length)
+    for(let i=1; i<=strArr[0].length; i++){
+        answer.push(strArr.filter((str) => str.length === i).length)
     }
     
     return Math.max(...answer);
