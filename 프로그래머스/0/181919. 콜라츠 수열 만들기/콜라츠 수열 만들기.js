b5e7626a7bbc039f2a71b295c54395af7e0d755a
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-06-06 10:32:22 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -3,8 +3,7 @@ function solution(n) {
     
     while(answer.at(-1) !== 1){
         const x = answer.at(-1);
-        const el = x % 2 === 0 ? x/2: (3*x)+1;
-        answer.push(el);
+        answer.push(x % 2 === 0 ? x / 2: (3 * x) + 1);
     }
     
     return answer;
