14414258da20c064c6715e3fcb6d7fb8841b4ba9
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-06-23 11:31:54 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -2,6 +2,5 @@ function solution(common) {
     const first = common[1] - common[0];
     const second = common[2] - common[1];
     const diff = second - first;
-        
     return diff === 0 ? common.at(-1) + first: common.at(-1) * second / first;
 }
