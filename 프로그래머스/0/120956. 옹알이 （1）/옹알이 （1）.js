bebf6334ad890d70ced958ead8d9787353361a26
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-07-21 11:41:35 +0900

    [level 0] Title: 옹알이 (1), Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,9 @@
+function solution(babbling) {
+    return babbling.reduce((a, c) => {
+        while(/^(aya|ye|woo|ma)/.test(c)){
+            if(/^(aya|woo)/.test(c)) c = c.slice(3);
+            if(/^(ye|ma)/.test(c)) c = c.slice(2);
+        }
+        return c.length === 0 ? a+1 : a;
+    }, 0);
+}
