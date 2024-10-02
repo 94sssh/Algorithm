4cfb59e894d3087fbb820ca4341d3969ad826741
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-10-02 17:11:24 +0900

    [level 2] Title: 주식가격, Time: 25.10 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,7 +1,7 @@
 function solution(prices) {
     const stack = new Array(prices.length).fill(0);
 
-    prices.map((price, idx) => {
+    prices.forEach((price, idx) => {
         for(let i = idx + 1; i < prices.length; i++){
             stack[idx]++;
             if(prices[i] < price) break;
