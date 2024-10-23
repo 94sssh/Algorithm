6fc94f80bff794d614c9aa065c6ca55c78c84f7f
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-10-23 14:49:56 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1026.46 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,14 +1,12 @@
 function solution(elements) {
     const answer = new Set();
     
-    const findSum = (arr, length) => {
-        for(let i = 0; i < elements.length; i++){
-            answer.add(arr.slice(i, i + length).reduce((a, c) => a + c, 0));
-        }
-    }
+    const arr = [...elements, ...elements];
     
-    elements.forEach((el, i) => {
-        findSum([...elements, ...elements], i + 1)
+    elements.forEach((_, i) => {
+        elements.forEach((_, idx) => {
+            answer.add(arr.slice(i, idx + i + 1).reduce((a, c) => a + c, 0));
+        })
     })
     
     return answer.size;
