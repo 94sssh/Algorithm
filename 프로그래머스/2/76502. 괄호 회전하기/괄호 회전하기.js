1483dfca1032890446c8ec4e04f54192bf7a65e0
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-10-25 11:31:06 +0900

    [level 2] Title: 괄호 회전하기, Time: 447.27 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,22 @@
+function solution(s) {
+    let answer = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        const stack = [];
+        
+        let startWithClose = false;
+        
+        s = s.slice(1) + s.slice(0, 1);
+        
+        for(let j = 0; j < s.length; j++){
+            if(s[j] === "[" || s[j] === "{" || s[j] === "(") stack.push(s[j]);
+            
+            if(stack.length === 0) startWithClose = true;
+                
+            if(s[j] === "]" && stack.at(-1) === "[" || s[j] === "}" && stack.at(-1) === "{" || s[j] === ")" && stack.at(-1) === "(") stack.pop();
+        };
+        
+        if(stack.length === 0 && !startWithClose) answer++;
+    };
+    return answer;
+};
