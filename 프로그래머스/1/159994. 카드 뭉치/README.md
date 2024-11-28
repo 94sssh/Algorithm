58e141a13c6a455315f25bc1e24a931600ba3cec
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-11-28 23:16:41 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.04 ms
+메모리: 33.5 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 28일 21:16:33
+2024년 11월 28일 23:16:38
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,7 +1,7 @@
 function solution(cards1, cards2, goal) {
-    for(let i=0; i<goal.length; i++){
-        if(cards1.indexOf(goal[i]) === 0) cards1.shift();
-        else if (cards2.indexOf(goal[i]) === 0) cards2.shift();
+    for(const card of goal){
+        if(cards1[0] === card) cards1.shift();
+        else if (cards2[0] === card) cards2.shift();
         else return 'No';
     };
     return 'Yes'
