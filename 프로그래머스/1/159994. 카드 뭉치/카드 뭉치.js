e269eec77fb47570082cd48f2f923d356094f46d
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2024-12-01 11:24:11 +0900

    [level 1] Title: 카드 뭉치, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,8 +1,18 @@
 function solution(cards1, cards2, goal) {
-    for(const card of goal){
-        if(cards1[0] === card) cards1.shift();
-        else if (cards2[0] === card) cards2.shift();
-        else return 'No';
-    };
-    return 'Yes'
+    let front = 0;
+    let rear = goal.length;
+    
+    while(front !== rear){
+        if(cards1[0] === goal[front]) {
+            cards1.shift();
+            front++;
+        } else if (cards2[0] === goal[front]) {
+            cards2.shift();
+            front++;
+        } else {
+            return 'No';
+        }
+    }
+    
+    return 'Yes';
 }
