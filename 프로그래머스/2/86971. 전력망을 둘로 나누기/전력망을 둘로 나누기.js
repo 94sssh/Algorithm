6b271f70e1248e3663d525cd07c4cfdda6220752
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2025-01-05 12:01:50 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 23.56 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -24,11 +24,7 @@ function solution(n, wires) {
         network[b].push(a);
     })
     
-    // wires에서 한 요소씩 반복하면서 간선 일시적으로 끊고 dfs 하기
     wires.forEach(([a, b]) => {
-        // network[a] = network[a].filter((el) => el !== b);
-        // network[b] = network[b].filter((el) => el !== a);
-        
         // 간선 빼서 나눈 그래프 towers를 dfs를 돌아서 나온 값(diff)를 answer랑 비교
         // 송전탑 개수
         const towers = dfs(a, b);
@@ -38,10 +34,6 @@ function solution(n, wires) {
         
         // diff랑 answer랑 비교해서 작은 값을 절대값으로 저장
         answer = diff < answer ? diff : answer;
-        
-        // 다시 간선 추가해서 복구
-        // network[a].push(b);
-        // network[b].push(a);
     })
     
     return answer;
