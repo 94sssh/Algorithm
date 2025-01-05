02d47f4632c2a48b4e9891f5ee2aed34795baa1e
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2025-01-05 12:00:42 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 22.69 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -2,20 +2,7 @@ function solution(n, wires) {
     let answer = n;
     const network = new Array(n + 1).fill(0).map(() => []);
     
-    
-    
-    // 먼저 그래프 만들기
-    wires.forEach(([a, b]) => {
-        network[a].push(b);
-        network[b].push(a);
-    })
-    
-    // wires에서 한 요소씩 반복하면서 간선 일시적으로 끊고 dfs 하기
-    wires.forEach(([a, b]) => {
-        network[a] = network[a].filter((el) => el !== b);
-        network[b] = network[b].filter((el) => el !== a);
-        
-        // dfs로 network를 돌면서 차이 구하기
+    // dfs로 network를 돌면서 차이 구하기
     // 간선이 하나씩 빠진 상태로 dfs 돌면서 나온 값을 계속해서 diff랑 비교
         
     // dfs를 시작할 parent 노드를 받고,
@@ -30,6 +17,17 @@ function solution(n, wires) {
             
         return line;
     }
+    
+    // 먼저 그래프 만들기
+    wires.forEach(([a, b]) => {
+        network[a].push(b);
+        network[b].push(a);
+    })
+    
+    // wires에서 한 요소씩 반복하면서 간선 일시적으로 끊고 dfs 하기
+    wires.forEach(([a, b]) => {
+        // network[a] = network[a].filter((el) => el !== b);
+        // network[b] = network[b].filter((el) => el !== a);
         
         // 간선 빼서 나눈 그래프 towers를 dfs를 돌아서 나온 값(diff)를 answer랑 비교
         // 송전탑 개수
@@ -42,8 +40,8 @@ function solution(n, wires) {
         answer = diff < answer ? diff : answer;
         
         // 다시 간선 추가해서 복구
-        network[a].push(b);
-        network[b].push(a);
+        // network[a].push(b);
+        // network[b].push(a);
     })
     
     return answer;
