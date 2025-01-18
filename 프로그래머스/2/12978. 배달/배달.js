11f1545e3730efa73e1357356c4cd62e0e961a88
SeungHo Han <93918946+94sssh@users.noreply.github.com>
2025-01-18 12:28:50 +0900

    [level 2] Title: 배달, Time: 6.36 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -14,10 +14,10 @@ function solution(N, road, K) {
     // 시작 노드의 직전 노드는 자신, 최소 비용은 0
     const queue = [[1, 0]];
     times[1] = 0;
-        
+            
     // 큐로 현재 마을에서 연결되어 있는 마을(아직 방문하지 않은 노드)을 순차적으로 돌면서 최소거리 갱신
     // 각 노드까지 가는 최소 비용과 현재까지 구한 최소 비용을 비교하여 작은 값을 각 노드의 최소 비용으로 갱신
-    while(queue.length) {
+    while(queue.length && times.slice(1).some((el) => el === Infinity)) {
         const [prevnode, prevtime] = queue.pop();
         
         town[prevnode].forEach(([nextnode, nexttime]) => {
